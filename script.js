eddaeae86e8505d9dfc4825ab8168841a13d7bb3
ABertie <U2439307@rts-365.dk>
2023-04-21 10:59:16 +0200

    style

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-const image_arr = [];
+const HEJ = (function() {
+    function init() {
+        
+    }
 
-function REQUEST() {
-    axios.request().then(function(res) {
-        for (let i = 0; i < response.data.results.length - 12; i++) {
-            image_arr.push(`https://image.tmdb.org/t/p/original/${response.data.results[i].poster_path}`)
 
-        }
-    })
-}
-
-REQUEST();
+})()
