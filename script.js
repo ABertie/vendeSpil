cce7cb67dba4af5a1f7ded1fbd2084689f80f8c1
ABertie <U2439307@rts-365.dk>
2023-04-21 11:34:17 +0200

    .

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-const HEJ = (function() {
-    function init() {
-        
+const IMAGES = []
+const CONTAINER = document.querySelector('.container')
+
+function REQUEST() {
+    for(let i = 0; i < 8; i++) {
+        IMAGES.push(`https://picsum.photos/30${i}`)
+        IMAGES.push(`https://picsum.photos/30${i}`)
     }
+}
 
+function RANDOMIZE() {
+    IMAGES.sort(function() {
+        return Math.random() - 0.5
+    })
+}
 
-})()
+function INSERT() {
+    for (let i = 0; i < IMAGES.length; i++) {
+        const IMG = document.createElement('img')
+
+        IMG.addEventListener('click', function(event) {
+            IMG.src = IMAGES[i]
+
+            console.log()
+        })
+
+        CONTAINER.appendChild(IMG)
+    }
+}
+
+REQUEST();
+
+RANDOMIZE();
+
+INSERT();
